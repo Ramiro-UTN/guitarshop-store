15fd7800b29742caaee8426154539741ada7245a
ramiro <102697125+Ramiro-UTN@users.noreply.github.com>
2023-12-23 07:03:48 -0300

    Add Gallery component

---
+++ components/gallery/gallery-tab.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Tab } from "@headlessui/react";
+
+import { cn } from "@/lib/utils";
+import {Image as ImageType} from "@/types";
+
+interface GalleryTabProps {
+  image: ImageType;
+}
+
+const GalleryTab: React.FC<GalleryTabProps> = ({
+  image
+}) => {
+  return (
+    <Tab className="relative aspect-square ">
+      {({selected}) => (
+        <div>
+          <span className="h-full w-full">
+            <Image
+              fill
+              src={image.url}
+              alt=""
+              className="object-cover"
+            />
+          </span>
+          <span className={cn(
+            "absolute inset-0 rounded-md ring-2",
+            selected ? "ring-black" : "ring-transparent"
+          )} />
+        </div>
+      )}
+    </Tab>
+  );
+}
+
+export default GalleryTab;
---
+++ components/gallery/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { Tab } from "@headlessui/react";
+
+import GalleryTab from "./gallery-tab";
+
+import { Image as ImageType } from "@/types";
+
+interface GalleryProps {
+  images: ImageType[];
+}
+
+
+const Gallery: React.FC<GalleryProps> = ({
+  images
+}) => {
+  return (
+    <Tab.Group as="div" className="flex flex-col">
+      <Tab.Panels>
+        {images.map((image) => (
+          <Tab.Panel key={image.id}>
+            <div className="aspect-square relative">
+              <Image
+                fill
+                src={image.url}
+                alt="Image"
+                className="object-cover"
+              />
+            </div>
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
+      <div className="mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+        <Tab.List className="grid grid-cols-4 gap-6">
+          {images.map((image) => (
+            <GalleryTab key={image.id} image={image} />
+          ))}
+        </Tab.List>
+      </div>
+    </Tab.Group>
+  );
+}
+
+export default Gallery;
